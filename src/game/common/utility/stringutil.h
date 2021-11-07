4e88a14ffa21225d44ede22efb8ebdf87375c06f
xezon <4720891+xezon@users.noreply.github.com>
2021-11-07 13:34:10 +0100

    Improve strip_whitespace functions

--- src/game/common/utility/stringutil.h
+++ src/game/common/utility/stringutil.h
@@ -23,29 +23,29 @@ namespace rts
 {
 // clang-format off
 
-template<typename CharType> constexpr CharType Get_Null();
-template<typename CharType> constexpr CharType Get_Tab();
-template<typename CharType> constexpr CharType Get_LF();
-template<typename CharType> constexpr CharType Get_VTab();
-template<typename CharType> constexpr CharType Get_FF();
-template<typename CharType> constexpr CharType Get_CR();
-template<typename CharType> constexpr CharType Get_Space();
+template<typename CharType> constexpr CharType get_null_char();
+template<typename CharType> constexpr CharType get_tab_char();
+template<typename CharType> constexpr CharType get_lf_char();
+template<typename CharType> constexpr CharType get_vtab_char();
+template<typename CharType> constexpr CharType get_ff_char();
+template<typename CharType> constexpr CharType get_cr_char();
+template<typename CharType> constexpr CharType get_space_char();
 
-template<> constexpr char Get_Null<char>()  { return '\0'; }
-template<> constexpr char Get_Tab<char>()   { return '\t'; }
-template<> constexpr char Get_LF<char>()    { return '\n'; }
-template<> constexpr char Get_VTab<char>()  { return '\v'; }
-template<> constexpr char Get_FF<char>()    { return '\f'; }
-template<> constexpr char Get_CR<char>()    { return '\r'; }
-template<> constexpr char Get_Space<char>() { return ' '; }
+template<> constexpr char get_null_char<char>()  { return '\0'; }
+template<> constexpr char get_tab_char<char>()   { return '\t'; }
+template<> constexpr char get_lf_char<char>()    { return '\n'; }
+template<> constexpr char get_vtab_char<char>()  { return '\v'; }
+template<> constexpr char get_ff_char<char>()    { return '\f'; }
+template<> constexpr char get_cr_char<char>()    { return '\r'; }
+template<> constexpr char get_space_char<char>() { return ' '; }
 
-template<> constexpr unichar_t Get_Null<unichar_t>()  { return U_CHAR('\0'); }
-template<> constexpr unichar_t Get_Tab<unichar_t>()   { return U_CHAR('\t'); }
-template<> constexpr unichar_t Get_LF<unichar_t>()    { return U_CHAR('\n'); }
-template<> constexpr unichar_t Get_VTab<unichar_t>()  { return U_CHAR('\v'); }
-template<> constexpr unichar_t Get_FF<unichar_t>()    { return U_CHAR('\f'); }
-template<> constexpr unichar_t Get_CR<unichar_t>()    { return U_CHAR('\r'); }
-template<> constexpr unichar_t Get_Space<unichar_t>() { return U_CHAR(' '); }
+template<> constexpr unichar_t get_null_char<unichar_t>()  { return U_CHAR('\0'); }
+template<> constexpr unichar_t get_tab_char<unichar_t>()   { return U_CHAR('\t'); }
+template<> constexpr unichar_t get_lf_char<unichar_t>()    { return U_CHAR('\n'); }
+template<> constexpr unichar_t get_vtab_char<unichar_t>()  { return U_CHAR('\v'); }
+template<> constexpr unichar_t get_ff_char<unichar_t>()    { return U_CHAR('\f'); }
+template<> constexpr unichar_t get_cr_char<unichar_t>()    { return U_CHAR('\r'); }
+template<> constexpr unichar_t get_space_char<unichar_t>() { return U_CHAR(' '); }
 
 // Whitespace is any of these:
 // ' ' (0x20)space(SPC)
@@ -54,28 +54,28 @@ template<> constexpr unichar_t Get_Space<unichar_t>() { return U_CHAR(' '); }
 // '\v'(0x0b)vertical tab(VT)
 // '\f'(0x0c)feed(FF)
 // '\r'(0x0d)carriage return (CR)
-template<typename CharType> constexpr bool Is_Whitespace(CharType ch)
+template<typename CharType> constexpr bool is_whitespace(CharType ch)
 {
 #ifdef THYME_USE_STLPORT
-    return ch == Get_Space<CharType>()
-        || ch == Get_Tab<CharType>()
-        || ch == Get_LF<CharType>()
-        || ch == Get_VTab<CharType>()
-        || ch == Get_FF<CharType>()
-        || ch == Get_CR<CharType>();
+    return ch == get_space_char<CharType>()
+        || ch == get_tab_char<CharType>()
+        || ch == get_lf_char<CharType>()
+        || ch == get_vtab_char<CharType>()
+        || ch == get_ff_char<CharType>()
+        || ch == get_cr_char<CharType>();
 #else
     return std::isspace(ch, std::locale());
 #endif
 }
 
-template<typename CharType> constexpr bool Is_Null(CharType ch)
+template<typename CharType> constexpr bool is_null(CharType ch)
 {
-    return ch == Get_Null<CharType>();
+    return ch == get_null_char<CharType>();
 }
 
-template<typename CharType> constexpr bool Is_Null_Or_Whitespace(CharType ch)
+template<typename CharType> constexpr bool is_null_or_whitespace(CharType ch)
 {
-    return Is_Null(ch) || Is_Whitespace(ch);
+    return is_null(ch) || is_whitespace(ch);
 }
 
 // clang-format on
@@ -88,112 +88,107 @@ enum StripOption
 };
 DEFINE_ENUMERATION_BITWISE_OPERATORS(StripOption);
 
-// Strips leading, trailing and duplicate whitespace. Returns count of stripped characters.
-// Before: "    Hello  there!   " After: "Hello there!"
-// Compatible with UTF-8 and UTF-16.
-template<typename CharType> int Strip_Whitespace(CharType *begin, CharType *end, StripOption option = STRIP_DEFAULT)
+// Strips leading, trailing and duplicate whitespace. Can provide options to customize the stripping result. Returns count of
+// stripped characters. Writes null over all stripped characters at the end. Compatible with UTF-8 and UTF-16.
+template<typename StringView> int strip_whitespace(StringView &string, StripOption option = STRIP_DEFAULT)
 {
-    captainslog_assert(end >= begin);
-    captainslog_assert((end - begin) < 0x0fff);
+    captainslog_assert(string.end() >= string.begin());
+    captainslog_assert((string.end() - string.begin()) < 0x0fff);
 
-    if (begin == end)
+    if (string.empty())
         return 0;
 
-    constexpr CharType null_char = Get_Null<CharType>();
-    CharType *reader = begin;
-    CharType *reader_end = end;
-    CharType *inserter = begin;
+    using char_type = typename StringView::value_type;
+    constexpr char_type null_char = get_null_char<char_type>();
+    char_type *reader = string.begin();
+    char_type *reader_end = string.end();
+    char_type *inserter = string.begin();
 
     const bool replace_whitespace = (option & STRIP_REPLACE_WHITESPACE) != 0;
     const bool leading_and_trailing_only = (option & STRIP_LEADING_AND_TRAILING_ONLY) != 0;
-    const bool is_null_terminated = Is_Null<CharType>(*(reader_end - 1));
 
     // Increment reader to first non-whitespace character.
-    for (; reader != reader_end && Is_Null_Or_Whitespace(*reader); ++reader) {
+    for (; reader != reader_end && is_null_or_whitespace(*reader); ++reader) {
     }
 
     // Decrement reader_end to first non-whitespace character.
-    for (; reader != reader_end && Is_Null_Or_Whitespace(*(reader_end - 1)); --reader_end) {
-    }
-
-    // The end was moved forward. If the original end was null terminated, then null will be put at new end.
-    if (is_null_terminated) {
-        *reader_end++ = null_char;
+    for (; reader != reader_end && is_null_or_whitespace(*(reader_end - 1)); --reader_end) {
     }
 
     while (reader != reader_end) {
-        CharType curr_char = *reader;
-        CharType next_char = (++reader != reader_end) ? *reader : null_char;
-        const bool curr_char_is_space = Is_Whitespace(curr_char);
+        char_type curr_char = *reader;
+        char_type next_char = (++reader != reader_end) ? *reader : null_char;
+        const bool curr_char_is_space = is_whitespace(curr_char);
 
         // If this and next character is any whitespace or null, then skip.
-        if (!leading_and_trailing_only && curr_char_is_space && Is_Null_Or_Whitespace(next_char)) {
+        if (!leading_and_trailing_only && curr_char_is_space && is_null_or_whitespace(next_char)) {
             continue;
         }
 
         // If this character is any whitespace, then turn it into a space character.
         if (replace_whitespace && curr_char_is_space) {
-            curr_char = Get_Space<CharType>();
+            curr_char = get_space_char<char_type>();
         }
 
         // Write out.
         *inserter++ = curr_char;
     }
 
-    return (reader - inserter) + (end - reader_end);
+    const int stripped_count = (reader - inserter) + (string.end() - reader_end);
+
+    // Fill the rest with null.
+    while (inserter != string.end()) {
+        *inserter++ = null_char;
+    }
+
+    // #TODO call a resize here if this is meant to be fully compatible with std::string like strings.
+
+    return stripped_count;
 }
 
-// Strips leading, trailing and duplicate whitespace. Returns count of stripped characters.
-// Before: "    Hello  there!   " After: "Hello there!"
-// Compatible with UTF-8 and UTF-16.
-template<typename StringType> int Strip_Whitespace(StringType &string, StripOption option = STRIP_DEFAULT)
+// Simplified algorithm for null terminated strings. Strips leading, trailing and duplicate whitespace. Returns count of
+// stripped characters. Writes null over all stripped characters at the end. Compatible with UTF-8 and UTF-16.
+template<typename CharType> int strip_all_whitespace(CharType *cstring)
 {
-    using CharType = typename StringType::value_type;
+    using char_type = CharType;
 
-    if ((option & STRIP_LEADING_AND_TRAILING_ONLY) != 0) {
-        // Run limited stripping mode. Requires full string length, including null terminator, to begin with.
+    constexpr char_type null_char = get_null_char<char_type>();
+    char_type *reader = cstring;
+    char_type *inserter = cstring;
 
-        CharType *begin = string.data();
-        CharType *end = string.data() + string.size() + 1;
-
-        return Strip_Whitespace(begin, end, option);
-    } else {
-        // Run full stripping mode. Optimized for null terminated string.
-
-        constexpr CharType null_char = Get_Null<CharType>();
-        CharType *reader = string.data();
-        CharType *inserter = string.data();
-
-        const bool replace_whitespace = (option & STRIP_REPLACE_WHITESPACE) != 0;
-
-        // Iterate to first non-whitespace character.
-        for (; *reader != null_char && Is_Whitespace(*reader); ++reader) {
-        }
-
-        while (*reader != null_char) {
-            CharType curr_char = *reader;
-            CharType next_char = *++reader;
-            const bool curr_char_is_space = Is_Whitespace(curr_char);
-
-            // If this and next character is any whitespace or null, then skip.
-            if (curr_char_is_space && Is_Null_Or_Whitespace(next_char)) {
-                continue;
-            }
-
-            // If this character is any whitespace, then turn it into a space character.
-            if (curr_char_is_space && replace_whitespace) {
-                curr_char = Get_Space<CharType>();
-            }
-
-            // Write out.
-            *inserter++ = curr_char;
-        }
-
-        // Close with null terminator always.
-        *inserter = null_char;
-
-        return (reader - inserter);
+    // Iterate to first non-whitespace character.
+    for (; *reader != null_char && is_whitespace(*reader); ++reader) {
     }
+
+    while (*reader != null_char) {
+        char_type curr_char = *reader;
+        char_type next_char = *++reader;
+        const bool curr_char_is_space = is_whitespace(curr_char);
+
+        // If this and next character is any whitespace or null, then skip.
+        if (curr_char_is_space && is_null_or_whitespace(next_char)) {
+            continue;
+        }
+
+        // If this character is any whitespace, then turn it into a space character.
+        if (curr_char_is_space) {
+            curr_char = get_space_char<char_type>();
+        }
+
+        // Write out.
+        *inserter++ = curr_char;
+    }
+
+    const int stripped_count = (reader - inserter);
+
+    // Fill the rest with null. Reader is the end by now.
+    while (inserter != reader) {
+        *inserter++ = null_char;
+    }
+
+    // #TODO call a resize here if this is meant to be fully compatible with std::string like strings.
+
+    return stripped_count;
 }
 
 } // namespace rts
