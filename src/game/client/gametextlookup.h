2d3688e5d914aab8b17b6293d0b32249447b7d47
xezon <4720891+xezon@users.noreply.github.com>
2021-12-17 21:44:44 +0100

    Simplify code in gametextlookup.h file

--- src/game/client/gametextlookup.h
+++ src/game/client/gametextlookup.h
@@ -14,9 +14,9 @@
  */
 #pragma once
 
-#include "always.h"
 #include "gametextcommon.h"
-#include <stdlib.h>
+#include "utility/stlutil.h"
+#include <cstdlib>
 
 namespace Thyme
 {
@@ -53,69 +53,50 @@ template<typename StringLookup, typename StringInfos> class GameTextLookup
 
 public:
     explicit GameTextLookup() {}
-    explicit GameTextLookup(StringInfos &stringInfos);
+    explicit GameTextLookup(StringInfos &stringInfos) { Load(stringInfos); }
 
-    void Load(StringInfos &stringInfos);
-    void Unload();
+    void Load(StringInfos &stringInfos)
+    {
+        const size_t size = stringInfos.size();
+        m_stringLookups.resize(size);
 
-    const StringLookup *Find(const char *label) const;
+        for (size_t i = 0; i < size; ++i) {
+            m_stringLookups[i].label = stringInfos[i].label.Str();
+            m_stringLookups[i].string_info = &stringInfos[i];
+        }
+
+        qsort(&m_stringLookups[0], size, sizeof(StringLookup), Compare_LUT);
+    }
+
+    void Unload() { rts::free_container(m_stringLookups); }
+
+    const StringLookup *Find(const char *label) const
+    {
+        StringLookup key;
+        key.label = label;
+        key.string_info = nullptr;
+
+        return static_cast<const StringLookup *>(
+            bsearch(&key, &m_stringLookups[0], m_stringLookups.size(), sizeof(StringLookup), Compare_LUT));
+    }
 
 private:
-    static int Compare_LUT(void const *a, void const *b);
+    static int Compare_LUT(void const *a, void const *b)
+    {
+        const char *ac = static_cast<const StringLookup *>(a)->label;
+        const char *bc = static_cast<const StringLookup *>(b)->label;
+
+        return strcasecmp(ac, bc);
+    }
 
     StringLookups m_stringLookups;
 };
 
-// Aliases.
+// Aliases for convenience.
+
 using ConstGameTextLookup = GameTextLookup<ConstStringLookup, const StringInfos>;
 using MutableGameTextLookup = GameTextLookup<MutableStringLookup, StringInfos>;
 using ConstMultiGameTextLookup = GameTextLookup<ConstMultiStringLookup, const MultiStringInfos>;
 using MutableMultiGameTextLookup = GameTextLookup<MutableMultiStringLookup, MultiStringInfos>;
-
-// Implementation.
-template<typename StringLookup, typename StringInfos>
-GameTextLookup<StringLookup, StringInfos>::GameTextLookup(StringInfos &stringInfos)
-{
-    Load(stringInfos);
-}
-
-template<typename StringLookup, typename StringInfos>
-void GameTextLookup<StringLookup, StringInfos>::Load(StringInfos &stringInfos)
-{
-    const size_t size = stringInfos.size();
-    m_stringLookups.resize(size);
-
-    for (size_t i = 0; i < size; ++i) {
-        m_stringLookups[i].label = stringInfos[i].label.Str();
-        m_stringLookups[i].string_info = &stringInfos[i];
-    }
-
-    qsort(&m_stringLookups[0], size, sizeof(StringLookup), Compare_LUT);
-}
-
-template<typename StringLookup, typename StringInfos> void GameTextLookup<StringLookup, StringInfos>::Unload()
-{
-    StringLookups().swap(m_stringLookups);
-}
-
-template<typename StringLookup, typename StringInfos>
-const StringLookup *GameTextLookup<StringLookup, StringInfos>::Find(const char *label) const
-{
-    StringLookup key;
-    key.label = label;
-    key.string_info = nullptr;
-
-    return static_cast<const StringLookup *>(
-        bsearch(&key, &m_stringLookups[0], m_stringLookups.size(), sizeof(StringLookup), Compare_LUT));
-}
-
-template<typename StringLookup, typename StringInfos>
-int GameTextLookup<StringLookup, StringInfos>::Compare_LUT(const void *a, const void *b)
-{
-    const char *ac = static_cast<const StringLookup *>(a)->label;
-    const char *bc = static_cast<const StringLookup *>(b)->label;
-
-    return strcasecmp(ac, bc);
-}
 
 } // namespace Thyme
